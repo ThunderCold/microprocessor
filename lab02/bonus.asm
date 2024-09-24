950967a92fb4dab3900909120dd7cdb2f4f9355f
Henry Tseng <55971876+ThunderCold@users.noreply.github.com>
2024-09-24 17:56:00 +0800

    Create bonus.asm

---
+++ lab02/bonus.asm
@@ -0,0 +1,64 @@
+List p=18f4520 ;???PIC18F4520
+    ;???PIC18F
+    #include<p18f4520.inc>
+        CONFIG OSC = INTIO67
+        CONFIG WDT = OFF
+        org 0x00 ; ???0x00???????
+
+MOVLW 0x28
+MOVWF 0x000
+MOVLW 0x34
+MOVWF 0x001
+MOVLW 0x7A
+MOVWF 0x002
+MOVLW 0x80
+MOVWF 0x003
+MOVLW 0xA7
+MOVWF 0x004
+MOVLW 0xD1
+MOVWF 0x005
+MOVLW 0xFE
+MOVWF 0x006
+MOVLW 0x89 ;目標值
+MOVWF 0x010
+
+LFSR 0, 0x000 ;FSR0=0x000
+LFSR 1, 0x006 ;FSR1=0x006
+	
+binarySearch:
+MOVF FSR1L, W
+CPFSGT FSR0L
+GOTO start
+GOTO not_found
+start:
+MOVF FSR0L, W ;計算中間位置
+ADDWF FSR1L, W
+RRCF WREG, W ;除以2
+MOVWF FSR2L ;FSR2=中間位置
+MOVF INDF2, W
+CPFSEQ 0x010 ;檢測目標值
+GOTO not_equal
+GOTO equal
+not_equal:
+CPFSLT 0x010 ;檢測目標值
+GOTO greater
+GOTO less
+equal:
+MOVLW 0xFF
+MOVWF 0x011
+GOTO theend
+greater:
+MOVF FSR2L, W
+MOVWF FSR0L ;更新左界
+INCF FSR0L
+GOTO binarySearch
+less:
+MOVF FSR2L, W
+MOVWF FSR1L ;更新右界
+DECF FSR1L
+GOTO binarySearch
+not_found:
+MOVLW 0x00
+MOVWF 0x011
+theend:
+end
