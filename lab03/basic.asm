674dfad817b7e2e458c8c74c0558a5efd090229e
Henry Tseng <55971876+ThunderCold@users.noreply.github.com>
2024-10-02 21:16:12 +0800

    Update basic.asm

--- lab03/basic.asm
+++ lab03/basic.asm
@@ -1,14 +1,18 @@
-List p=18f4520 ;設備是PIC18F4520
-    ;初始化PIC18F
+List p=18f4520 ;???PIC18F4520
+    ;???PIC18F
     #include<p18f4520.inc>
         CONFIG OSC = INTIO67
         CONFIG WDT = OFF
-        org 0x00 ; 程式從0x00的位置開始執行
+        org 0x00 ; ???0x00???????
 
-MOVLW 0x5F
-MOVWF 0x000
-RLNCF 0x000
-BTFSC 0x000, 7
+MOVLW 0xC8
+MOVWF TRISA
+
+MOVLW b'11111110'
+RLNCF TRISA
+ANDWF TRISA, F
+	
+BTFSC TRISA, 7
 GOTO one
 GOTO zero
 one:
@@ -17,7 +21,7 @@ GOTO or
 zero:
 MOVLW 0x00
 or:
-RRNCF 0x000
-IORWF 0x000, F
+RRNCF TRISA
+IORWF TRISA, F
 
 end
