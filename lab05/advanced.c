9d2ef6d9f8451fa2977a8807b2ae02a20fb92a0a
Henry Tseng <55971876+ThunderCold@users.noreply.github.com>
2024-10-24 02:30:58 +0800

    Update advanced.c

--- lab05/advanced.c
+++ lab05/advanced.c
@@ -2,7 +2,7 @@
 extern unsigned int gcd(unsigned int a, unsigned int b);
 
 void main(void) {
-    volatile unsigned int result = gcd(25, 25);
+    volatile unsigned int result = gcd(1200, 180);
     while(1);
     return;
 }
